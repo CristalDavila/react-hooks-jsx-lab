4740dbe0bd69965cf75d95300c9f0bcf8cfa38b1
Cristal Davila <cristal.davila@gmail.com>
2023-08-22 21:48:30 -0600

    passes tests

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
-}
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>
+      Nooow, this is a story all about how
+      My life got flipped, Turned upside down!
+      And I'd like to take a minute
+      Just sit right there,
+      I'll tell you how I became the Prince of a town called Bel Air...
+    </p>
+    <img src={image} alt="I made this" />
+  </div>
+};
 
 export default About;
